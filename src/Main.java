8f081e9d4796dc966ed4e93d57a37e878ae8a3df
Milton-commitarson <milton.molanderryberg@elev.ga.ntig.se>
2024-01-12 12:47:02 +0100

    2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
+/*Todo
+Classer:
+* Spel
+* spelare
+* Kort
+färg, nummer, och specialkort
+* SpelFunktion
+
+ */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
